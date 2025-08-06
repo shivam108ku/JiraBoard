5c2baaca8e00ee556120be21cd3a9df3fe2c8a7b
Shivam Kumar <shivam108ku@gmail.com>
2025-08-06 14:42:51 +0530

    add functionality

--- src/components/TaskForm/task-form.jsx
+++ src/components/TaskForm/task-form.jsx
@@ -1,34 +1,54 @@
 import "./task-form.css";
 import Tag from "../Tag/Tag";
+import { useState } from "react";
 
 const TaskForm = () => {
-    return (
-        <header className="app_header">
-            <form>
-                
-                <input type='text' placeholder='Enter Class Details' className='task_input' />
+    const [task , setTask] = useState("");
+    const [status , setStatus] = useState("");
 
-                <div className='task_form_bottom'>
-                    <div>
-                        <Tag tagName="DEV"/>
-                        <Tag tagName="QA" />
-                        <Tag tagName="Product Owner" />
-                    </div>
-                    <div>
-                        <select className='task_status'>
-                            <option value="Ready For Development">Ready For Development</option>
-                            <option value="Task In Progress">Task In Progress</option>
-                            <option value="Ready For Test">Ready For Test</option>
-                            <option value="Closed">Closed</option>
-                        </select>
-                        <button type='submit' className='task_submit'>+Add</button>
-                    </div>
-                </div>
+    const handleTask = (e) => {
+        setTask(e.target.value)
+    }
 
-            </form>
+    const handleStatusChange = (e) => {
+        setStatus(e.target.value);
+    }
+    
+    console.log(status);
+    console.log(task)
+  return (
+    <header className="app_header">
+      <form>
+        <input
+          type="text"
+          placeholder="Enter Class Details"
+          className="task_input"
+          onChange={handleTask}
+        />
 
-        </header>
-    )
-}
+        <div className="task_form_bottom">
+          <div>
+            <Tag tagName="DEV" />
+            <Tag tagName="QA" />
+            <Tag tagName="Product Owner" />
+          </div>
+          <div>
+            <select className="task_status" onChange={handleStatusChange}>
+              <option value="Ready For Development">
+                Ready For Development
+              </option>
+              <option value="Task In Progress">Task In Progress</option>
+              <option value="Ready For Test">Ready For Test</option>
+              <option value="Closed">Closed</option>
+            </select>
+            <button type="submit" className="task_submit">
+              +Add
+            </button>
+          </div>
+        </div>
+      </form>
+    </header>
+  );
+};
 
-export default TaskForm
+export default TaskForm;
